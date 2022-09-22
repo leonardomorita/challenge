58c498c982176f332e1a0d4de097bb53361913a6
leonardomorita <leonardomorita2@gmail.com>
2022-09-22 07:32:50 -0300

    Post list

--- resources/views/public.blade.php
+++ resources/views/public.blade.php
@@ -1,26 +1,28 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Postagens</div>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Postagens</div>
 
-                <div class="card-body">
-                    <b>|| Adicione aqui as postagens ativas ||</b>
+                    <div class="card-body">
+                        {{-- <b>|| Adicione aqui as postagens ativas ||</b> --}}
 
-                    <div class="card" style="width: 18rem;">
-                        <img src="..." class="card-img-top" alt="...">
-                        <div class="card-body">
-                            <h5 class="card-title">Título</h5>
-                            <p class="card-text">Descrição</p>
-                            <a href="{{ URL::to('postagem') }}" class="btn btn-primary">Abrir postagem</a>
-                        </div>
+                        @foreach ($postagens as $postagem)
+                            <div class="card" style="width: 18rem;">
+                                <img src={{ asset("storage/images/$postagem->imagem") }} class="card-img-top" alt="foto-{{ $postagem->id }}">
+                                <div class="card-body">
+                                    <h5 class="card-title">{{ $postagem->titulo }}</h5>
+                                    <p class="card-text">{{ $postagem->descricao }}</p>
+                                    <a href="{{ route('public.postagem', ['id' => $postagem->id]) }}" class="btn btn-primary">Abrir postagem</a>
+                                </div>
+                            </div>
+                        @endforeach
                     </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -4,5 +4,5 @@ Auth::routes();
 Route::get('/posts/novo', 'PostsController@novo');
 Route::post('/posts/criar', 'PostsController@criar')->name('posts.criar');
 Route::get('/home', 'HomeController@index')->name('home');
-Route::get('/postagem/', 'PublicController@postagem');
+Route::get('/postagem/{id}', 'PublicController@postagem')->name('public.postagem');
 Route::get('/', 'PublicController@index');
